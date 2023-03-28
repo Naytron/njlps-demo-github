7d2994febd047c63c83fe2692bad434835d6cd90
Nathan Langston <nlangston@microsoft.com>
2023-03-28 11:57:35 -0600

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,5 @@
+﻿Console.WriteLine("------------------------------------------------");
+Console.WriteLine(" NJ LPS - Demo ");
+Console.WriteLine("------------------------------------------------");
+Console.ReadLine();
+
